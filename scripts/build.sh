3d8ad61ec3e7591c31375e8975e53fde6dfa4a91
David O'Sullivan <davidos@vmware.com>
2022-02-28 15:52:54 +0000

    Added module version

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 GOOS="linux" go build -ldflags='-s -w' -o bin/helper github.com/paketo-buildpacks/libjvm/cmd/helper
-GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/paketo-buildpacks/azul-zulu/cmd/main
+GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/paketo-buildpacks/azul-zulu/v9/cmd/main
 
 if [ "${STRIP:-false}" != "false" ]; then
   strip bin/helper bin/main
